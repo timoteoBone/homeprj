2cfd94a03396584869235147b0b5e4a6ad7a58a7
Timoteo Bone <TB254821@fi365.ort.edu.uy>
2021-12-15 16:19:08 -0300

    [red] se crea prueba de fizzBuzz, falta corregir el metodo

---
+++ FizzBuzz/FizzBuzzLogic.go
@@ -0,0 +1,9 @@
+package fizzbuzz
+
+var fizzRes string = "fizz"
+var buzzRes string = "buzz"
+var FizzBuzzRes string = "FizzBuzz"
+
+func FizzBuzz(num int) string {
+	return "now"
+}
---
+++ FizzBuzz/FizzBuzz_test.go
@@ -0,0 +1,50 @@
+package fizzbuzz
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFizzNumber(t *testing.T) {
+	testCases := []struct {
+		name           string
+		input          int
+		expectedResult string
+	}{
+		{
+			name:           "Case of Fizz alone",
+			input:          3,
+			expectedResult: fizzRes,
+		},
+
+		{
+			name:           "Case of Buzz alone",
+			input:          5,
+			expectedResult: buzzRes,
+		},
+
+		{
+			name:           "Case of FizzBuzz",
+			input:          15,
+			expectedResult: FizzBuzzRes,
+		},
+
+		{
+			name:           "Case of not Fizz nor Buzz",
+			input:          4,
+			expectedResult: "4",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			result := FizzBuzz(tc.input)
+
+			assert.Equal(t, tc.expectedResult, result)
+
+		})
+	}
+
+}
