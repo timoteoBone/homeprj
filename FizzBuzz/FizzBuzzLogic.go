3aad661bd29a9da73cd5222e97305bb4a783ec8c
Timoteo Bone <TB254821@fi365.ort.edu.uy>
2021-12-15 16:29:24 -0300

    [green][refactor]se implementa correctamente metodo fizzbuzz

--- FizzBuzz/FizzBuzzLogic.go
+++ FizzBuzz/FizzBuzzLogic.go
@@ -1,9 +1,33 @@
 package fizzbuzz
 
+import "strconv"
+
 var fizzRes string = "fizz"
 var buzzRes string = "buzz"
 var FizzBuzzRes string = "FizzBuzz"
 
 func FizzBuzz(num int) string {
-	return "now"
+	var res string
+	fizz := dividendBy3(num)
+	buzz := dividendBy5(num)
+
+	if fizz && buzz {
+		res = FizzBuzzRes
+	} else if fizz && !buzz {
+		res = fizzRes
+	} else if buzz && !fizz {
+		res = buzzRes
+	} else {
+		res = strconv.Itoa(num)
+	}
+	return res
+}
+
+func dividendBy3(num int) bool {
+	return num%3 == 0
+
+}
+
+func dividendBy5(num int) bool {
+	return num%5 == 0
 }
