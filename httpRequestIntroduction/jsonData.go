02c0ba68f2850c4580689b4f75a5037326ed965b
Timoteo Bone <TB254821@fi365.ort.edu.uy>
2021-12-16 22:02:21 -0300

    first http request

---
+++ httpRequestIntroduction/jsonData.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := string(body)
+	log.Printf(data)
+
+}
